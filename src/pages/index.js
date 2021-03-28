ad96d650038fe6a1a1dd5f6896eca9489849f51f
jhahspu <jhahspu@outlook.com>
2021-03-28 19:33:39 +0100

    home page grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,28 +9,29 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="home">
-      <div className="home--text">
-        <div>
-          <h1>Design</h1>
-          <h1>Develop</h1>
-          <h1>Deploy</h1>
-          <Link to="/projects" className="btn-action"> My Projects</Link>
-        </div>
+
+      <div id="hero">
+        <h1>Design</h1>
+        <h1>Develop</h1>
+        <h1>Deploy</h1>
+        <Link to="/projects" id="btn-action"> My Projects</Link>
       </div>
-      <div className="home--images">
-        <img
-          id="phone"
-          src={require(`../images/phone.svg`).default}
-          alt="phone" />
-        <img
-          id="desktop"
-          src={require(`../images/desktop.svg`).default}
-          alt="desktop" />
-        <img
-          id="server"
-          src={require(`../images/server.svg`).default}
-          alt="server" />
-      </div>
+      
+      
+      <img
+        id="phone"
+        src={require(`../images/phone.svg`).default}
+        alt="phone" />
+
+      <img
+        id="desktop"
+        src={require(`../images/desktop.svg`).default}
+        alt="desktop" />
+
+     <img
+        id="server"
+        src={require(`../images/server.svg`).default}
+        alt="server" />
     </div>
   </Layout>
 )
